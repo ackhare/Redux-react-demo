fb77e7aac605162317dc1b0fe84544b14e5aafde
chetan <bairagisirfire@gmail.com>
2017-07-18 13:00:16 +0530

    basic react app with no redux and data is cominf from a static source

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {render} from "react-dom";
+
+
+import ItemList from "./components/ItemList";
+
+
+render(
+    <ItemList />,
+    document.getElementById('app')
+);
