b7d8dd49eac64ad904b6ba406cc8f01a42510f19
Khare <Chetan.Khare@CenturyLink.com>
2019-03-01 12:41:22 +0530

    a demo of react routes with useful links and g docs linkl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import {render} from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App';
+
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 
-import ItemList from "./components/ItemList";
-
-
-render(
-    <ItemList />,
-    document.getElementById('app')
-);
